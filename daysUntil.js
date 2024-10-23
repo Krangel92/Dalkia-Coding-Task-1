1f52cb2ce3a8bf445d523565c82caff2e150bc73
Karla Rangel <krangelhdz@gmail.com>
2024-10-23 15:14:13 +0100

    further changes of syntax to modern JS

--- daysUntil.js
+++ daysUntil.js
@@ -1,20 +1,20 @@
 const calculateDaysUntilChristmas = () => {
-	var p = new Date();
-	var q = new Date("2024-12-25");
+	let p = new Date();
+	let q = new Date("2024-12-25");
 
-	var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+	let variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
 
-	var variable2 = document.getElementById("my-element");
+	let variable2 = document.getElementById("my-element");
 	variable2.innerText = variable;
 };
 
 const calculateDaysUntilNewYear = () => {
-	var p = new Date();
-	var q = new Date("2025-01-01");
+	let p = new Date();
+	let q = new Date("2025-01-01");
 
-	var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+	let variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
 
-	var variable2 = document.getElementById("my-element2");
+	let variable2 = document.getElementById("my-element2");
 	variable2.innerText = variable;
 };
 
