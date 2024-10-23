455bf70c6445e1d9f39324e4f755242e4457cff9
Karla Rangel <krangelhdz@gmail.com>
2024-10-23 15:21:21 +0100

    changed the variable names of p and q for both functions to more meaniningful names

--- daysUntil.js
+++ daysUntil.js
@@ -1,18 +1,22 @@
 const calculateDaysUntilChristmas = () => {
-	let p = new Date();
-	let q = new Date("2024-12-25");
+	let todaysDate = new Date();
+	let christmasDate = new Date("2024-12-25");
 
-	let variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+	let variable = Math.round(
+		(christmasDate - todaysDate) / (1000 * 60 * 60 * 24)
+	);
 
 	let variable2 = document.getElementById("my-element");
 	variable2.innerText = variable;
 };
 
 const calculateDaysUntilNewYear = () => {
-	let p = new Date();
-	let q = new Date("2025-01-01");
+	let todaysDate = new Date();
+	let newYearsdate = new Date("2025-01-01");
 
-	let variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+	let variable = Math.round(
+		(newYearsdate - todaysDate) / (1000 * 60 * 60 * 24)
+	);
 
 	let variable2 = document.getElementById("my-element2");
 	variable2.innerText = variable;
