5a1103bd3940843e395d3306c1d0b2da956a1553
Karla Rangel <krangelhdz@gmail.com>
2024-10-23 15:31:02 +0100

    further varibale name changes (for the calculation)

--- daysUntil.js
+++ daysUntil.js
@@ -2,24 +2,24 @@ const calculateDaysUntilChristmas = () => {
 	let todaysDate = new Date();
 	let christmasDate = new Date("2024-12-25");
 
-	let variable = Math.round(
+	let calculationResult = Math.round(
 		(christmasDate - todaysDate) / (1000 * 60 * 60 * 24)
 	);
 
 	let variable2 = document.getElementById("my-element");
-	variable2.innerText = variable;
+	variable2.innerText = calculationResult;
 };
 
 const calculateDaysUntilNewYear = () => {
 	let todaysDate = new Date();
 	let newYearsdate = new Date("2025-01-01");
 
-	let variable = Math.round(
+	let calculationResult = Math.round(
 		(newYearsdate - todaysDate) / (1000 * 60 * 60 * 24)
 	);
 
 	let variable2 = document.getElementById("my-element2");
-	variable2.innerText = variable;
+	variable2.innerText = calculationResult;
 };
 
 calculateDaysUntilChristmas();
