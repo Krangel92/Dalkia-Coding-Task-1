2f649cd8c676a2b2ccc2a36f6dd3c894f45c1c4c
Karla Rangel <krangelhdz@gmail.com>
2024-10-25 10:19:43 +0100

    updated code for the calculateDaysUntilChristmas function to make it dynamic

--- daysUntil.js
+++ daysUntil.js
@@ -1,6 +1,12 @@
 const calculateDaysUntilChristmas = () => {
 	let todaysDate = new Date();
-	let christmasDate = new Date("2024-12-25");
+	let currentYear = todaysDate.getFullYear();
+
+	let christmasDate = new Date(currentYear, 11, 25);
+
+	if (todaysDate > christmasDate) {
+		christmasDate = new Date(currentYear + 1, 11, 25);
+	}
 
 	let calculationResult = Math.round(
 		(christmasDate - todaysDate) / (1000 * 60 * 60 * 24)
